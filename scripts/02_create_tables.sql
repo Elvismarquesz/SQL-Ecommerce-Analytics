9415e35bdb4e7f7848a73681fef615132bd4f8dd
elvis-marques <elvis.marques37@gmail.com>
2026-07-23 18:18:33 -0300

    Cria tabelas customers e orders

--- scripts/02_create_tables.sql
+++ scripts/02_create_tables.sql
@@ -16,3 +16,31 @@ CREATE TABLE customers (
     customer_state CHAR(2) NOT NULL
 
 );
+
+-- =====================================================
+-- Tabela: orders
+-- Descrição: Armazena os pedidos realizados pelos clientes
+-- =====================================================
+
+CREATE TABLE orders (
+
+    order_id VARCHAR(32) PRIMARY KEY,
+
+    customer_id VARCHAR(32) NOT NULL,
+
+    order_status VARCHAR(20) NOT NULL,
+
+    order_purchase_timestamp TIMESTAMP NOT NULL,
+
+    order_approved_at TIMESTAMP,
+
+    order_delivered_carrier_date TIMESTAMP,
+
+    order_delivered_customer_date TIMESTAMP,
+
+    order_estimated_delivery_date TIMESTAMP,
+
+    FOREIGN KEY (customer_id)
+        REFERENCES customers(customer_id)
+
+);
