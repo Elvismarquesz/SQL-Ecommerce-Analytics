0241c660945f71949b49f2a2f0756ce9f3b24847
elvis-marques <elvis.marques37@gmail.com>
2026-07-23 17:41:59 -0300

    Cria tabela customers

---
+++ scripts/02_create_tables.sql
@@ -0,0 +1,18 @@
+-- =====================================================
+-- Tabela: customers
+-- Descrição: Armazena informações dos clientes
+-- =====================================================
+
+CREATE TABLE customers (
+
+    customer_id VARCHAR(32) PRIMARY KEY,
+
+    customer_unique_id VARCHAR(32) NOT NULL,
+
+    customer_zip_code_prefix INTEGER NOT NULL,
+
+    customer_city VARCHAR(100) NOT NULL,
+
+    customer_state CHAR(2) NOT NULL
+
+);
