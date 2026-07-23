9283eb227baaff48e5148b788116abb2ec39a41a
elvis-marques <elvis.marques37@gmail.com>
2026-07-23 17:07:05 -0300

    Adiciona script de criação do banco de dados

---
+++ scripts/01_create_database.sql
@@ -0,0 +1,7 @@
+-- =====================================================
+-- Projeto: SQL E-commerce Analytics
+-- Autor: Elvis Marques
+-- Descrição: Criação do banco de dados do projeto
+-- =====================================================
+
+CREATE DATABASE ecommerce_analytics;
