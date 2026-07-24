7910bfa8ab1db2a9ba7c87390a1caf491a95b694
elvis-marques <elvis.marques37@gmail.com>
2026-07-24 18:26:41 -0300

    Cria a tabela sellers

--- scripts/02_create_tables.sql
+++ scripts/02_create_tables.sql
@@ -71,3 +71,21 @@ CREATE TABLE products (
     product_width_cm NUMERIC
 
 );
+
+-- =====================================================
+-- Tabela: sellers 
+-- Descrição: Armazena informações dos vendedores
+-- =====================================================
+
+CREATE TABLE sellers (
+
+    seller_id VARCHAR(32) PRIMARY KEY,
+
+    seller_zip_code_prefix INTEGER NOT NULL,
+
+    seller_city VARCHAR(100) NOT NULL,
+
+    seller_state CHAR(2) NOT NULL
+
+);
+
